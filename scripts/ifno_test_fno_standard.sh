4dea66694ebedff202352b0cf36acc8fc5081d74
Robert Joseph George <robujsph2001@gmail.com>
2023-06-27 20:58:27 -0700

    done

--- scripts/ifno_test_fno_standard.sh
+++ scripts/ifno_test_fno_standard.sh
@@ -9,21 +9,28 @@ do
             --priority NORMAL \
             --preempt RUNONCE \
             --ace nv-us-west-2 \
-            --instance dgx1v.32g.4.norm \
+            --instance dgx1v.16g.1.norm \
             --image nvcr.io/nvidian/nvr-aialgo/fly-incremental:zoo_latest \
             --result /results \
             --workspace 6Ubcqvn_Rn6uKFJw4ijJdw:/ngc_workspace \
             --datasetid 23145:/dataset \
+            --datasetid 110516:/high_res_ns_dataset \
             --team nvr-aialgo \
             --port 6006 --port 1234 --port 8888 \
             --commandline "bash -c '\
                 sh /ngc_workspace/jiawei/set_wandb.sh; \
+                pip install configmypy zarr mpi4py; \
+                pip install -U tensorly; \
+                pip install -U tensorly-torch ; \
                 cd /workspace; \
                 git clone https://github.com/Robertboy18/neuraloperator.git; \
+                cd /workspace/neuraloperator; \
+                pip install -e . ; \
+                cp /ngc_workspace/jiawei/wandb_api_key.txt config/wandb_api_key.txt; \
                 cd /workspace/neuraloperator/scripts; \
                 git checkout robert-test-incremental; \
                 cp -r /ngc_workspace/jiawei/projects/ifno/data /workspace/fly-incremental/data; \
-                mpiexec --allow-run-as-root -n 8 python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR ;\
+                python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR ;\
             '"
     done
 done
@@ -39,22 +46,29 @@ do
             --priority NORMAL \
             --preempt RUNONCE \
             --ace nv-us-west-2 \
-            --instance dgx1v.32g.4.norm \
+            --instance dgx1v.16g.1.norm \
             --image nvcr.io/nvidian/nvr-aialgo/fly-incremental:zoo_latest \
             --result /results \
             --workspace 6Ubcqvn_Rn6uKFJw4ijJdw:/ngc_workspace \
             --datasetid 23145:/dataset \
+            --datasetid 110516:/high_res_ns_dataset \
             --team nvr-aialgo \
             --port 6006 --port 1234 --port 8888 \
             --commandline "bash -c '\
                 sh /ngc_workspace/jiawei/set_wandb.sh; \
+                pip install configmypy zarr mpi4py; \
+                pip install -U tensorly; \
+                pip install -U tensorly-torch ; \
                 cd /workspace; \
                 git clone https://github.com/Robertboy18/neuraloperator.git; \
+                cd /workspace/neuraloperator; \
+                pip install -e . ; \
+                cp /ngc_workspace/jiawei/wandb_api_key.txt config/wandb_api_key.txt; \
                 cd /workspace/neuraloperator/scripts; \
                 git checkout robert-test-incremental; \
                 cp -r /ngc_workspace/jiawei/projects/ifno/data /workspace/fly-incremental/data; \
-                mpiexec --allow-run-as-root -n 8 python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR --incremental.incremental_resolution.use=True --incremental.incremental_grad.use=True --incremental.incremental_grad.grad_explained_ratio_threshold=$THRESHOLD;\
-                mpiexec --allow-run-as-root -n 8 python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR --incremental.incremental_grad.use=True --incremental.incremental_grad.grad_explained_ratio_threshold=$THRESHOLD;\
+                python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR --incremental.incremental_resolution.use=True --incremental.incremental_grad.use=True --incremental.incremental_grad.grad_explained_ratio_threshold=$THRESHOLD;\
+                python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR --incremental.incremental_grad.use=True --incremental.incremental_grad.grad_explained_ratio_threshold=$THRESHOLD;\
             '"
     done
 done
@@ -69,22 +83,29 @@ do
             --priority NORMAL \
             --preempt RUNONCE \
             --ace nv-us-west-2 \
-            --instance dgx1v.32g.4.norm \
+            --instance dgx1v.16g.1.norm \
             --image nvcr.io/nvidian/nvr-aialgo/fly-incremental:zoo_latest \
             --result /results \
             --workspace 6Ubcqvn_Rn6uKFJw4ijJdw:/ngc_workspace \
             --datasetid 23145:/dataset \
+            --datasetid 110516:/high_res_ns_dataset \
             --team nvr-aialgo \
             --port 6006 --port 1234 --port 8888 \
             --commandline "bash -c '\
                 sh /ngc_workspace/jiawei/set_wandb.sh; \
+                pip install configmypy zarr mpi4py; \
+                pip install -U tensorly; \
+                pip install -U tensorly-torch ; \
                 cd /workspace; \
                 git clone https://github.com/Robertboy18/neuraloperator.git; \
+                cd /workspace/neuraloperator; \
+                pip install -e . ; \
+                cp /ngc_workspace/jiawei/wandb_api_key.txt config/wandb_api_key.txt; \
                 cd /workspace/neuraloperator/scripts; \
                 git checkout robert-test-incremental; \
                 cp -r /ngc_workspace/jiawei/projects/ifno/data /workspace/fly-incremental/data; \
-                mpiexec --allow-run-as-root -n 8 python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR --incremental.loss_gap.use=True --incremental.loss_gap.eps=$EPS;\
-                mpiexec --allow-run-as-root -n 8 python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR --incremental.incremental_resolution.use=True --incremental.loss_gap.use=True --incremental.loss_gap.eps=$EPS;\
+                python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR --incremental.loss_gap.use=True --incremental.loss_gap.eps=$EPS;\
+                python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR --incremental.incremental_resolution.use=True --incremental.loss_gap.use=True --incremental.loss_gap.eps=$EPS;\
             '"
     done
 done
@@ -100,21 +121,28 @@ do
             --priority NORMAL \
             --preempt RUNONCE \
             --ace nv-us-west-2 \
-            --instance dgx1v.32g.4.norm \
+            --instance dgx1v.16g.1.norm \
             --image nvcr.io/nvidian/nvr-aialgo/fly-incremental:zoo_latest \
             --result /results \
             --workspace 6Ubcqvn_Rn6uKFJw4ijJdw:/ngc_workspace \
             --datasetid 23145:/dataset \
+            --datasetid 110516:/high_res_ns_dataset \
             --team nvr-aialgo \
             --port 6006 --port 1234 --port 8888 \
             --commandline "bash -c '\
                 sh /ngc_workspace/jiawei/set_wandb.sh; \
+                pip install configmypy zarr mpi4py; \
+                pip install -U tensorly; \
+                pip install -U tensorly-torch ; \
                 cd /workspace; \
                 git clone https://github.com/Robertboy18/neuraloperator.git; \
+                cd /workspace/neuraloperator; \
+                pip install -e . ; \
+                cp /ngc_workspace/jiawei/wandb_api_key.txt config/wandb_api_key.txt; \
                 cd /workspace/neuraloperator/scripts; \
                 git checkout robert-test-incremental; \
                 cp -r /ngc_workspace/jiawei/projects/ifno/data /workspace/fly-incremental/data; \
-                mpiexec --allow-run-as-root -n 8 python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR --incremental.incremental_resolution.use=True;\
+                python train_navier_stokes.py --opt.scheduler="StepLR" --opt.learning_rate=$LR --incremental.incremental_resolution.use=True;\
             '"
     done
 done
