d811fca8dd199a4a8eae435b2d68821d94dd5d51
Robert Joseph <robujsph2001@gmail.com>
2023-08-01 18:17:32 -0700

    done

--- scripts/ifno_test_fno.sh
+++ scripts/ifno_test_fno.sh
@@ -1,9 +1,8 @@
+TASK_NAME=ifno_batch_script_test_fno_different_modes_resolution_and_loss_gap_test_modes1
 
-TASK_NAME=ifno_batch_script_test_fno_different_modes_resolution_and_loss_gap_test_modes5
-
-for BASE_LR in 1e-4
+for BASE_LR in 1e-5
 do
-    for MAX_LR in 1e-3
+    for MAX_LR in 5e-4
     do
         ngc batch run \
             --name "ml-model.$TASK_NAME" \
@@ -30,7 +29,79 @@ do
                 cd /workspace/neuraloperator/scripts; \
                 git checkout robert-test-incremental; \
                 cp -r /ngc_workspace/jiawei/projects/ifno/data /workspace/fly-incremental/data; \
-                python train_navier_stokes.py --checkpoint.name="checkpoints5";\
+                python train_navier_stokes.py --opt.mode="triangular2" --opt.base_lr=$BASE_LR --opt.max_lr=$MAX_LR --incremental.incremental_resolution.use=True --checkpoint.name="checkpoints12";\
+            '"
+    done
+done
+
+TASK_NAME=ifno_batch_script_test_fno_different_modes_resolution_and_loss_gap_test_modes2
+
+for BASE_LR in 1e-5
+do
+    for MAX_LR in 5e-4
+    do
+        ngc batch run \
+            --name "ml-model.$TASK_NAME" \
+            --preempt RUNONCE \
+            --ace nv-us-west-2 \
+            --instance dgx1v.32g.1.norm \
+            --image nvcr.io/nvidian/nvr-aialgo/fly-incremental:zoo_latest \
+            --result /results \
+            --workspace 6Ubcqvn_Rn6uKFJw4ijJdw:/ngc_workspace \
+            --datasetid 23145:/dataset \
+            --datasetid 110516:/high_res_ns_dataset \
+            --team nvr-aialgo \
+            --port 6006 --port 1234 --port 8888 \
+            --commandline "bash -c '\
+                sh /ngc_workspace/jiawei/set_wandb.sh; \
+                pip install configmypy zarr mpi4py; \
+                pip install -U tensorly; \
+                pip install -U tensorly-torch ; \
+                cd /workspace; \
+                git clone https://github.com/Robertboy18/neuraloperator.git; \
+                cd /workspace/neuraloperator; \
+                pip install -e . ; \
+                cp /ngc_workspace/jiawei/wandb_api_key.txt config/wandb_api_key.txt; \
+                cd /workspace/neuraloperator/scripts; \
+                git checkout robert-test-incremental; \
+                cp -r /ngc_workspace/jiawei/projects/ifno/data /workspace/fly-incremental/data; \
+                python train_navier_stokes.py --opt.mode="triangular" --opt.base_lr=$BASE_LR --opt.max_lr=$MAX_LR --incremental.incremental_resolution.use=True --checkpoint.name="checkpoints13";\
+            '"
+    done
+done
+
+TASK_NAME=ifno_batch_script_test_fno_different_modes_resolution_and_loss_gap_test_modes3
+
+for BASE_LR in 1e-5
+do
+    for MAX_LR in 5e-4
+    do
+        ngc batch run \
+            --name "ml-model.$TASK_NAME" \
+            --preempt RUNONCE \
+            --ace nv-us-west-2 \
+            --instance dgx1v.32g.1.norm \
+            --image nvcr.io/nvidian/nvr-aialgo/fly-incremental:zoo_latest \
+            --result /results \
+            --workspace 6Ubcqvn_Rn6uKFJw4ijJdw:/ngc_workspace \
+            --datasetid 23145:/dataset \
+            --datasetid 110516:/high_res_ns_dataset \
+            --team nvr-aialgo \
+            --port 6006 --port 1234 --port 8888 \
+            --commandline "bash -c '\
+                sh /ngc_workspace/jiawei/set_wandb.sh; \
+                pip install configmypy zarr mpi4py; \
+                pip install -U tensorly; \
+                pip install -U tensorly-torch ; \
+                cd /workspace; \
+                git clone https://github.com/Robertboy18/neuraloperator.git; \
+                cd /workspace/neuraloperator; \
+                pip install -e . ; \
+                cp /ngc_workspace/jiawei/wandb_api_key.txt config/wandb_api_key.txt; \
+                cd /workspace/neuraloperator/scripts; \
+                git checkout robert-test-incremental; \
+                cp -r /ngc_workspace/jiawei/projects/ifno/data /workspace/fly-incremental/data; \
+                python train_navier_stokes.py --opt.mode="exp_range" --opt.base_lr=$BASE_LR --opt.max_lr=$MAX_LR --incremental.incremental_resolution.use=True --checkpoint.name="checkpoints14";\
             '"
     done
 done
