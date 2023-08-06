2e632767dd730664bf9d43e1c4f75c1f1265783f
Robert Joseph George <robujsph2001@gmail.com>
2023-08-06 13:07:21 -0700

    done

--- neuralop/datasets/hdf5_dataset.py
+++ neuralop/datasets/hdf5_dataset.py
@@ -51,8 +51,8 @@ class H5pyDataset(Dataset):
         #print(self.data.keys())
         #print(self.data['512x512x2_wn1.0']['fields'])64x64x2_wn1.0
         self.subsample_step = 1
-        x = self.data['512x512x2_wn16.0']['fields'][idx, 1:, ::self.subsample_step, ::self.subsample_step]
-        y = self.data['512x512x2_wn16.0']['fields'][idx+1, 1:, ::self.subsample_step, ::self.subsample_step]
+        x = self.data['64x64x2_wn1.0']['fields'][idx, :1, ::self.subsample_step, ::self.subsample_step]
+        y = self.data['64x64x2_wn1.0']['fields'][idx+1, :1, ::self.subsample_step, ::self.subsample_step]
         
         x = torch.tensor(x, dtype=torch.float32)
         y = torch.tensor(y, dtype=torch.float32)
