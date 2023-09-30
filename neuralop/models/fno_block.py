3dc60e157b158335f0a34c158a6f181b02e63557
Robert Joseph <robujsph2001@gmail.com>
2023-09-30 00:15:04 -0700

    done

--- neuralop/models/fno_block.py
+++ neuralop/models/fno_block.py
@@ -42,7 +42,7 @@ def _contract_dense(x, weight, separable=False, train_resolution=32, mode = "tra
     if train_resolution < test_resolution and mode == "test":
         #print("Test ", x.shape, weight.shape, train_resolution, previous_shape)
         x = x[: , :, :train_resolution, :previous_shape]
-        weight = weight[:, :, :32, :32]
+        weight = weight[:, :, :32, :previous_shape]
         #print("Changed", x.shape, weight.shape)
     return tl.einsum(eq, x, weight)
 
