5f0d729c4cd85b9ea7c93ac269a22daa8c2e28ae
Robert Joseph <robujsph2001@gmail.com>
2023-10-20 15:36:27 -0700

    done

--- neuralop/training/trainer.py
+++ neuralop/training/trainer.py
@@ -271,9 +271,9 @@ class Trainer:
             if isinstance(scheduler[0], torch.optim.lr_scheduler.ReduceLROnPlateau):
                 scheduler[0].step(train_err)
             else:
-                if epoch <= 180:
+                if epoch <= 150:
                     scheduler[1].step()
-                elif epoch > 190:
+                elif epoch > 180:
                     scheduler[1].step()
                 else:
                     scheduler[0].step()
